46ae030737ac99e15e50318df2ea597b0106d38c
Anandhu H <anandhu@techvantagesystems.com>
2024-09-05 16:43:24 +0530

    llama70B

--- chat/views.py
+++ chat/views.py
@@ -1,26 +1,9 @@
-from django.http import JsonResponse, StreamingHttpResponse
+from django.http import StreamingHttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from .llama_model import Llama3
-
 import json
-bot = Llama3("meta-llama/Meta-Llama-3.1-8B-Instruct")
 
-# @csrf_exempt 
-# def chatbot_view(request):
-#     if request.method == "POST":
-#         user_input = request.POST.get("input", "")
-#         response = StreamingHttpResponse(
-#             bot._chatbot(user_input),
-#             content_type='text/plain',
-#         )
-        
-#         return response
-
-
-
-
-bot = Llama3("meta-llama/Meta-Llama-3.1-8B-Instruct")
-#bot = Llama3("meta-llama/Meta-Llama-3.1-70B-Instruct")
+bot = Llama3("meta-llama/Meta-Llama-3.1-70B-Instruct")
 
 @csrf_exempt
 def chatbot_view(request):
